7b5221487f2f6149905afc64d53d2fe75a209e7f
Kevin CF <71736322+Christian0286@users.noreply.github.com>
2022-10-13 21:44:35 -0500

    Add files via upload

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,71 +1,111 @@
 <template>
-<!-- bootstrap form -->
-<form @submit.prevent="registrarproyecto">
-    <div class="mb-3">
-      <label for="" class="form-label">Proyecto</label>
-      <input v-model.trim="proyecto" type="text" class="form-control" required>
-    </div>
-    <div class="mb-3">
-        <label for="" class="form-label">Actividad</label>
-        <select class="form-select" required  v-model.trim="tipo">
-            <option disabled selected value="">Seleccione tipo de actividad</option>
-            <option value="1">Aplicaciones Web con Vue.JS</option>
-            <option value="2">Backend Service con Node.js</option>
-            <option value="3">App Movil con React Native</option>
-        </select>
-    </div>
-    <div class="mb-3">
-        <label class="form-check-label m-1">Urgente</label>
-        <input type="checkbox" class="form-check-input m-2" v-model.trim="urgente">
-    </div>
-    <button type="submit" class="btn btn-primary">Guardar</button>
-  </form>
-  <hr>
-  <div class="table-responsive">
-    <table class="table table-primary text-center">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Proyecto</th>
-                <th>Tipo</th>
-                <th>Urgente</th>
-            </tr>
-        </thead>
-        <tbody v-for="(proyecto,index) in proyectos" :key="index">
-            <tr class="table-light">
-                <td>{{index + 1}}</td>
-                <td>{{proyecto.proyecto}}</td>
-                <td>{{proyecto.tipo}}</td>
-                <td :class="proyecto.urgente ? 'bg-success' : 'bg-danger'">{{proyecto.urgente ? "Si" : "No"}}</td>
-            </tr>
-        </tbody>
-    </table>
+
+  <div class="row"> 
+    <div class="col-12 mb-4">
+      <progress-bar :porcentaje="porcentaje"/>
+  </div>
+      <!-- bootstrap form -->
+      <div class="row">
+        <form @submit.prevent="registrarProyecto" class="col-12 col-md-4 mt-3">
+          <div class="mb-3">
+            <label for="" class="form-label">Proyecto</label>
+            <input v-model.trim="proyecto" type="text" class="form-control" required>
+          </div>
+          <div class="mb-3">
+              <label for="" class="form-label">Actividad</label>
+              <select class="form-select" required  v-model.trim="tipo">
+                  <option disabled selected value="">Seleccione tipo de actividad</option>
+                  <option value="1">Aplicaciones Web con Vue.JS</option>
+                  <option value="2">Backend Service con Node.js</option>
+                  <option value="3">App Movil con React Native</option>
+              </select>
+          </div>
+          <div class="mb-3">
+              <label class="form-check-label m-1">Urgente</label>
+              <input type="checkbox" class="form-check-input m-2" v-model.trim="urgente">
+          </div>
+            <button type="submit" class="btn btn-primary">Guardar</button>
+        </form>
+        <div class="col-12 col-md-8">
+          <total-proyectos 
+          :numeroProyectos="numeroProyectos" 
+          :proyectos="proyectos" 
+          :cambiarEstado="cambiarEstado"
+          :limpiarData="limpiarData"/>
+        </div>
+          </div>
   </div>
 </template>
 
 <script>
+  import TotalProyectos from './TotalProyectos.vue';
+  import ProgressBar from './ProgressBar.vue'
     export default{
+      components:{
+        TotalProyectos,
+        ProgressBar
+      },
       data:()=>({
         proyecto:"",
         tipo:"",
         urgente:false,
-        proyectos:[]
+        proyectos:[],
+        numeroProyectos:0,
       }),
       methods:{
-        registrarproyecto(){
+        registrarProyecto(){
             const proyecto = {
                 proyecto: this.proyecto,
                 tipo : this.tipo,
-                urgente : this.urgente
+                urgente : this.urgente,
+                completado : false
             };
-
-        this.proyectos.push(proyecto);
+          
+        this.proyectos.push(proyecto),
+        this.numeroProyectos++;
         this.proyecto = "",
         this.tipo = "",
         this.urgente = false,
-          console.log(this.proyectos)
-        }
+        console.log(this.proyectos)
+        },
+        
+        
+        cambiarEstado(proyecto,campo){
+          //this.urgente = !this.urgente;
+          //this.proyectos[id].urgente = !this.proyectos[id].urgente;
+          proyecto[campo] = !proyecto[campo];
+          this.saveData();
+        },
+        
+        saveData(){
+          this.proyecto = JSON.stringify(localStorage.getItem("proyectos"))
+        },
+
+        limpiarData(){
+          this.proyectos = [];
+          localStorage.clear();
+        },
+  
       },
+      computed:{
+        numeroProyectos(){
+          return this.proyectos.length;
+        },
+        porcentaje(){
+          let completados = 0;
+          
+          this.proyectos.map(proyecto =>{
+            if (proyecto.completado) 
+              completados++;
+            
+          });
+          return (completados * 100) / this.numeroProyectos || 0;
+        },
+        mounted(){
+        this.proyectos =  JSON.parse(localStorage.getItem("proyectos"))
+      },
+      },
+      
     };
   
   </script>
